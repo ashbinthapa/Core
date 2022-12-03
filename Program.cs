51c9ec909b29f1d99b7f7d181bea4d18eb628145
Ashbin Thapa <thapasmit82@gmail.com>
2022-12-03 11:58:40 +0545

    cookies

--- Program.cs
+++ Program.cs
@@ -1,91 +1,30 @@
-using Core;
-using Core.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
-/*
-builder.Services.Configure<FruitOptions>(options =>
-{
-    options.Name = "watermelon";
-});
-*/
-
-builder.Services.AddTransient<IResponseFormatter, GuidService>();
 var app = builder.Build();
 
-/*
-app.Use(async (context, next) =>
+app.MapGet("/cookie", async context =>
 {
-    if(context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
-    {
-        context.Response.ContentType = "text/plain";
-        await context.Response.WriteAsync("Custom Middleware\n");
-    }
-    await next();
-});
-*/
-
-
-/*
-app.Use(async (context, next) =>
-{   
-    if(context.Request.Path == "/short")
-    {
-        await context.Response.WriteAsync("Request-short-circuited");
-    }
-    else
-    {
-        await next();
-    }
+    int counter = int.Parse(context.Request.Cookies["counter"] ?? "0") + 1;
+    context.Response.Cookies.Append(
+        "counter",
+        counter.ToString(),
+        new CookieOptions
+        {
+            MaxAge = TimeSpan.FromSeconds(100)
+        }
+    );
+    await context.Response.WriteAsync($"Cookie:{counter}");
+    
 });
 
-*/
-
-
-
-/*
-((IApplicationBuilder)app).Map("/branch", branch =>
+app.MapGet("/clear", context =>
 {
-    branch.Run(async (HttpContext context) =>
-    {
-        await context.Response.WriteAsync("Branch middleware");
-    });
+    context.Response.Cookies.Delete("counter");
+    context.Response.Redirect("/");
+    return Task.CompletedTask;
+
 });
-*/
-
-/*
-((IApplicationBuilder)app).Map("/branch", branch =>
-{
-    branch.Run(new Middleware().Invoke);
-});
-*/
-
-//app.MapGet("/fruit", async(HttpContext context, IOptions<FruitOptions> FruitOptions )=>
-//{   
-//    FruitOptions options = FruitOptions.Value;
-//    await context.Response.WriteAsync($"{options.Name}, {options.Color}");
-//});
-//app.UseMiddleware<FruitMiddleware>();
-
-//IResponseFormatter formatter = new TextResponseFormatter();
-
-app.MapGet("/formatter1", async (HttpContext context, IResponseFormatter formatter) =>
-{
-    await formatter.Format(context, "Fromatter 1");
-});
-
-app.MapGet("/formatter2", async (HttpContext context, IResponseFormatter formatter) =>
-{
-    await formatter.Format(context, "Fromatter 2");
-});
-
-
-app.UseMiddleware<CustomMiddleware>();
-app.UseMiddleware<CustomMiddleware2>();
-
-app.MapGet("/endpoint", CustomEndpoint.EndPoint);
 app.MapGet("/", () => "Hello World!");
-
-
 app.Run();
