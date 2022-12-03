9637a2b46a72349c149f5254a5a9b59eb9c301dd
Ashbin Thapa <thapasmit82@gmail.com>
2022-12-03 12:20:15 +0545

    https enforcing

--- Program.cs
+++ Program.cs
@@ -3,26 +3,16 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout= TimeSpan.FromSeconds(5);
-    options.Cookie.IsEssential= true;
-});
 var app = builder.Build();
-app.UseSession();
 
-app.MapGet("/session", async context =>
+app.MapGet("/http", async context =>
 {
-    int counter = (context.Session.GetInt32("counter") ?? 0) + 1;
-
-    context.Session.SetInt32("counter", counter);
-
-    await context.Session.CommitAsync();
-
-    await context.Response.WriteAsync($"Session:{counter}");
-    
+    await context.Response.WriteAsync($"HTTPS Request: {context.Request.IsHttps}");
 });
+
+//enforcong https
+app.UseHttpsRedirection();
+
 
 app.MapGet("/", () => "Hello World!");
 app.Run();
