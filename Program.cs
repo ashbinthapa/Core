d2ba6d2f88abea2906d8d4e16c40d4cb922f6878
Ashbin Thapa <thapasmit82@gmail.com>
2022-12-03 12:08:46 +0545

    session

--- Program.cs
+++ Program.cs
@@ -2,29 +2,27 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/cookie", async context =>
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
 {
-    int counter = int.Parse(context.Request.Cookies["counter"] ?? "0") + 1;
-    context.Response.Cookies.Append(
-        "counter",
-        counter.ToString(),
-        new CookieOptions
-        {
-            MaxAge = TimeSpan.FromSeconds(100)
-        }
-    );
-    await context.Response.WriteAsync($"Cookie:{counter}");
+    options.IdleTimeout= TimeSpan.FromSeconds(5);
+    options.Cookie.IsEssential= true;
+});
+var app = builder.Build();
+app.UseSession();
+
+app.MapGet("/session", async context =>
+{
+    int counter = (context.Session.GetInt32("counter") ?? 0) + 1;
+
+    context.Session.SetInt32("counter", counter);
+
+    await context.Session.CommitAsync();
+
+    await context.Response.WriteAsync($"Session:{counter}");
     
 });
 
-app.MapGet("/clear", context =>
-{
-    context.Response.Cookies.Delete("counter");
-    context.Response.Redirect("/");
-    return Task.CompletedTask;
-
-});
 app.MapGet("/", () => "Hello World!");
 app.Run();
